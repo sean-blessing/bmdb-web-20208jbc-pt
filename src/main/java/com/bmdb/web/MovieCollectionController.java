931539aef67a71a651d374ee3b7224d6ee4e40fd
Sean Blessing <snblessing@gmail.com>
2020-11-18 20:52:42 -0500

    added recalculate functionality

--- src/main/java/com/bmdb/web/MovieCollectionController.java
+++ src/main/java/com/bmdb/web/MovieCollectionController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.bmdb.business.MovieCollection;
+import com.bmdb.business.User;
 import com.bmdb.db.MovieCollectionRepo;
+import com.bmdb.db.UserRepo;
 
 @CrossOrigin
 @RestController
@@ -24,6 +26,8 @@ public class MovieCollectionController {
 	
 	@Autowired
 	private MovieCollectionRepo movieCollectionRepo;
+	@Autowired
+	private UserRepo userRepo;
 	
 	//Get all movieColllections
 	@GetMapping("/")
@@ -38,20 +42,38 @@ public class MovieCollectionController {
 	}
 	
 	//Add a movieColllection
+	//Also recalculate the collection value in User
 	@PostMapping("/")
 	public MovieCollection addMovieCollection(@RequestBody MovieCollection m) {
 		m = movieCollectionRepo.save(m);
+		recalculateCollectionValue(m);
 		return m;
+	}
+
+	private void recalculateCollectionValue(MovieCollection m) {
+		// get all movie collections for this user
+		// loop through them and sum a new total
+		double newTotal = 0.0;
+		List<MovieCollection> mcs = movieCollectionRepo.findByUserId(m.getUser().getId());
+		for (MovieCollection mc: mcs) {
+			newTotal += mc.getPurchasePrice();
+		}
+		User u = m.getUser();
+		u.setCollectionValue(newTotal);
+		userRepo.save(u);
 	}
 	
 	//Update a movieColllection
+	//Also recalculate the collection value in User
 	@PutMapping("/")
 	public MovieCollection updateMovieCollection(@RequestBody MovieCollection m) {
 		m = movieCollectionRepo.save(m);
+		recalculateCollectionValue(m);
 		return m;
 	}
 	
 	//Delete a movieColllection
+	//Also recalculate the collection value in User
 	@DeleteMapping("/{id}")
 	public MovieCollection deleteMovieCollection(@PathVariable int id) {
 		//Optional type will wrap a movieColllection
@@ -59,6 +81,7 @@ public class MovieCollectionController {
 		//isPresent() will return true if a movieColllection was found
 		if (m.isPresent()) {
 			movieCollectionRepo.deleteById(id);
+			recalculateCollectionValue(m.get());
 		}
 		else {
 			System.out.println("Error - movie collection not found for id "+id);
